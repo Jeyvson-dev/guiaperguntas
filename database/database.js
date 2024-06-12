bc5e23354b0d7e4687b860f99b0c56b309bce64a
Jeyvson Moreira <jeyvson0000@gmail.com>
2024-06-12 18:39:21 -0300

    feat: Question page

--- database/database.js
+++ database/database.js
@@ -4,4 +4,6 @@ require('dotenv').config();
 const connection = new Sequelize(process.env.DATABASE, process.env.USER_DATABASE, process.env.PASSWROD_DATABASE, {
     host: process.env.HOST_DATABASE,
     dialect: process.env.DIALECT
-})
+});
+
+module.exports = connection;
--- index.js
+++ index.js
@@ -1,18 +1,38 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const connection = require('./database/database');
 const port = 8080;
+const Pergunta = require("./database/Pergunta");
 
+//database
+connection.authenticate()
+.then(() =>{
+    console.log("Conexão feita com o banco de dados");
+})
+.catch((msgErro)=>{
+    console.log(msgErro);
+});
 
+//views
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+
 //Body Parser
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(bodyParser.json());
 
+
 //Routes
 app.get("/", (req, res) => {
-    res.render("index");
+
+    Pergunta.findAll({ raw: true, order: [
+        ['id', 'desc']
+    ] }).then(perguntas =>{
+        res.render("index",{
+            perguntas: perguntas,
+        });
+    });  
 });
 
 app.get("/perguntar", (req, res) => {
@@ -23,7 +43,32 @@ app.get("/perguntar", (req, res) => {
 app.post('/salvarpergunta', (req, res)=>{
     var titulo = req.body.titulo;
     var descricao = req.body.descricao;
-    res.send("titulo: " + titulo + " Descrição: " + descricao);
+    Pergunta.create({
+        titulo: titulo,
+        description: descricao
+    }).then(()=>{
+        console.log("usuário criado com sucesso");
+        res.redirect("/");
+    });
+})
+
+app.get('/pergunta/:id', (req, res)=>{
+    var id = req.params.id;
+
+    Pergunta.findOne({
+        where :{id: id},
+        raw: true,
+    }).then(pergunta =>{
+        if(pergunta != undefined){
+            console.log(pergunta)
+            res.render("pergunta",{
+                pergunta: pergunta,
+            });
+        }else{
+            res.redirect("/");
+        } 
+    })
+
 })
 
 app.listen(port, () => {
